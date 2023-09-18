7e1c140f9c0408310e072a8168781f3598ea715f
ynsinan <yns.inan1660@gmail.com>
2023-09-18 23:38:37 +0300

    describe information

---
+++ src/components/button/button.test.js
@@ -0,0 +1,12 @@
+describe('Button Component', () => {
+   describe('when variant primary', () => {
+        test("...", () => {});
+        test("....", () => {});
+        test(".....", () => {});
+    });
+    describe('when variant secondary', () => {
+        test("...", () => {});
+        test("....", () => {});
+        test(".....", () => {});
+    });
+});
---
+++ src/components/button/index.js
@@ -0,0 +1,7 @@
+const Button = ({children, variant="primary"}) => {
+  return (
+    <button variant={variant}>{children}</button>
+  )
+}
+
+export default Button
