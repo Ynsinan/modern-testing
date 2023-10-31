b2edfc8ccd1b6aa585e340d0841f6f0792d8138e
Yunus İNAN <yunus.inan@demirorenteknoloji.com>
2023-10-31 22:52:49 +0300

    copy test

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { useState } from 'react';
 
 function MyComponent() {
-  const [count,setCount] = useState(0);
 
-  const handleIncrement = () => {
-    setCount(count + 1);
+  const handleCopy = () => {
+    window.navigator.clipboard.writeText("Hello World");
   };
 
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleCopy}>Copy</button>
+      <input type='text' />
     </div>
   );
 }
@@ -20,14 +18,15 @@ function MyComponent() {
 it('should wait for loading message to be removed', async () => {
   const user = userEvent.setup();
   render(<MyComponent />);
-
-  const buttonElement = screen.getByRole('button', { name: "Increment" });
   
-  await user.keyboard("[Tab]");
-  expect(buttonElement).toHaveFocus();
+  await user.keyboard("[tab]");
+  await user.keyboard("[enter]");
 
-  await user.keyboard("[Enter]");
-  const headingElement = await screen.findByText('1');
-  expect(headingElement).toBeInTheDocument();
+  const inputElement = screen.getByRole('textbox');
+  inputElement.focus();
+
+  await user.paste();
+
+  expect(inputElement).toHaveValue('Hello World');
 
 });
